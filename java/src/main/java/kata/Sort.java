5c29f75347be076d72ef2129350e7a2bf8fc34b6
Mihai Gătejescu <mgatejescu@pm.me>
2026-07-18 19:11:06 +0300

    Add the insert sort algorithm

--- java/src/main/java/kata/Sort.java
+++ java/src/main/java/kata/Sort.java
@@ -3,6 +3,10 @@ package kata;
 public class Sort
 {
     public static int[] bubbleSort(int[] nums) {
+        return nums;
+    }
 
+    public static int[] insertSort(int[] nums) {
+        return nums;
     }
 }
--- java/src/test/java/kata/SortTest.java
+++ java/src/test/java/kata/SortTest.java
@@ -1,6 +1,7 @@
 package kata;
 
 import static kata.Sort.bubbleSort;
+import static kata.Sort.insertSort;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
@@ -33,6 +34,36 @@ public class SortTest
         int[] nums = { 5 };
 
         int[] actual = bubbleSort(nums);
+        int[] expected = { 5 };
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testInsertSort() {
+        int[] nums = { 5, 3, 8, 4, 2 };
+
+        int[] actual = insertSort(nums);
+        int[] expected = { 2, 3, 4, 5, 8 };
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testInsertSortEmpty() {
+        int[] nums = {};
+
+        int[] actual = insertSort(nums);
+        int[] expected = {};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testInsertSortOneElement() {
+        int[] nums = { 5 };
+
+        int[] actual = insertSort(nums);
         int[] expected = { 5 };
 
         assertArrayEquals(expected, actual);
