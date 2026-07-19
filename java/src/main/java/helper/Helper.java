0bf3306c722ae967f1cde3e210958a498529456e
Mihai Gătejescu <mgatejescu@pm.me>
2026-07-19 19:18:11 +0300

    Add a helper method to print arrays

---
+++ java/src/main/java/helper/Helper.java
@@ -0,0 +1,17 @@
+package helper;
+
+
+public class Helper {
+
+    public static void printArray(int[] nums) {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        for (int i = 0; i < nums.length; ++i) {
+            result.append(nums[i]);
+            result.append((i < nums.length-1) ? ", " : "");
+        }
+        result.append("]");
+
+        System.out.println(result);
+    }
+}
