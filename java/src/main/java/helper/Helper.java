a9dc3c43899f18186a77d8b745f36e28615b7ea4
Mihai Gătejescu <mgatejescu@pm.me>
2026-07-21 20:46:54 +0300

    Add swap methods

--- java/src/main/java/helper/Helper.java
+++ java/src/main/java/helper/Helper.java
@@ -4,6 +4,18 @@ package helper;
 @SuppressWarnings("unused")
 public class Helper {
 
+    public static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    public static void arithmeticSwap(int[] nums, int first, int second) {
+        nums[first] = nums[first] + nums[second];
+        nums[second] = nums[first] - nums[second];
+        nums[first] = nums[first] - nums[second];
+    }
+
     public static void printArray(int[] nums) {
         StringBuilder result = new StringBuilder();
         result.append("[");
